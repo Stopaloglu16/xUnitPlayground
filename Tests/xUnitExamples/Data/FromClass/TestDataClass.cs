1b4bb03788c256e916ff5e3895480a6a9520e0f2
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2023-03-01 21:58:47 +0000

    InlineData, ClassData, MemberData

---
+++ Tests/xUnitExamples/Data/FromClass/TestDataClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitExamples.Data.FromClass
+{
+
+    public record RecordForTestData(int Input, int ExpectedResult);
+
+
+    public class TestDataClass : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new()
+    {
+        new object[] { new RecordForTestData(1, 2) },
+        new object[] { new RecordForTestData(4,8) },
+        new object[] { new RecordForTestData(7, 14) }
+    };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+
+  
+
+
+
+
+}
--- Tests/xUnitExamples/UnitTest1.cs
+++ Tests/xUnitExamples/UnitTest1.cs
@@ -1,9 +1,26 @@
+using xUnitExamples.Data.FromClass;
+
 namespace xUnitExamples
 {
     public class UnitTest1
     {
         [Fact]
-        public void ShouldReturnSum()
+        public void SimpleTest_DataAssign_TruePass()
+        {
+            //arrange
+            var fieldVal1 = 1;
+            var fieldVal2 = 2;
+
+            //act
+            var sum0 = fieldVal1 + fieldVal2;
+
+            //assert
+            Assert.True(3 == sum0, "Result is not correct");
+        }
+
+
+        [Fact]
+        public void SimpleTest_DataAssign_EqualPass()
         {
             //arrange
             var fieldVal1 = 1;
@@ -14,8 +31,54 @@ namespace xUnitExamples
 
             //assert
             Assert.Equal(3, sum0);
+        }
 
+
+        /// <summary>
+        /// The theory attribute to pass simple parameters to the test case.
+        /// </summary>
+        [Theory]
+        [InlineData(2, 3, 5)]
+        public void SimpleTest_InlineData_EqualPass(int fieldVal1, int fieldVal2, int expect)
+        {
+            //act
+            var sum0 = fieldVal1 + fieldVal2;
+
+            //assert
+            Assert.Equal(expect, sum0);
+        }
+
+        /// <summary>
+        /// If the test case takes the type as a parameter
+        /// Also, One of the advantage is clean code and reusability.
+        /// </summary>
+        [Theory]
+        [ClassData(typeof(TestDataClass))]
+        public void DoubleUp_ClassData_EqualPass(RecordForTestData data)
+        {
+            Assert.Equal(data.Input, data.ExpectedResult / 2);
+        }
+
+
+        /// <summary>
+        /// For loading the complex data for the test cases. Any static property or method can be assigned to the XUnit memberdata attribute
+        /// The [MemberData] attribute can be used to fetch data for a [Theory] from a static property or method of a type
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(TestDataWithObjects))]
+        public void DoubleUp_MemberData_EqualPass(RecordForTestData data)
+        {
+            Assert.Equal(data.Input, data.ExpectedResult / 2);
 
         }
+
+        public static IEnumerable<object[]> TestDataWithObjects => new[]
+        {
+           new object[] { new RecordForTestData(1, 2) },
+           new object[] { new RecordForTestData(4,8) },
+           new object[] { new RecordForTestData(7, 14) }
+        };
+
+
     }
 }
