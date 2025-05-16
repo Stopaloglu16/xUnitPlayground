5184f1f96cb4ff2176209cf1a9d57f925550c9b3
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2025-05-16 21:49:24 +0100

    Add xUnitPlaywrightExamples project and setup instructions
    - Introduced a new project "xUnitPlaywrightExamples" to the solution.
    - Added `HowtoSetup.md` with Playwright installation and PowerShell update instructions.

---
+++ Tests/xUnitPlaywrightExamples/Info/HowtoSetup.md
@@ -0,0 +1,31 @@
+﻿## How to install Playwright
+
+Go to the project directory and run the following command to install Playwright.
+
+```powershell
+pwsh .\bin\Debug\net9.0\playwright.ps1 install
+```
+<i>
+If pwsh gives you an error, try to update powershell to the latest version.
+</i>
+
+
+## Powershell update steps
+
+### Show current version of PowerShell
+```powershell
+$psversiontable
+```
+
+### command should return the latest version of PowerShell
+```powershell
+winget search Microsoft.PowerShell
+```
+
+### For installations, you can use the following command to install PowerShell using winget:
+```powershell
+winget install --id Microsoft.PowerShell --source winget
+```
+
+
+
---
+++ Tests/xUnitPlaywrightExamples/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright.Xunit;
+using Microsoft.Playwright;
+
+namespace xUnitPlaywrightExamples
+{
+
+    public class UnitTest1 : PageTest
+    {
+        [Fact]
+        public async Task TestWithCustomContextOptions()
+        {
+            // The following Page (and BrowserContext) instance has the custom colorScheme, viewport and baseURL set:
+            await Page.GotoAsync("/login");
+        }
+        public override BrowserNewContextOptions ContextOptions()
+        {
+            return new BrowserNewContextOptions()
+            {
+                ColorScheme = ColorScheme.Light,
+                ViewportSize = new()
+                {
+                    Width = 1920,
+                    Height = 1080
+                },
+                BaseURL = "https://github.com",
+            };
+        }
+    }
+}
